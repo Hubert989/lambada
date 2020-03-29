c2a8957fd76fdb7299ce7b9804d7c8b13ab6f19f
hubcio619@onet.pl <hubcio619@onet.pl>
2020-03-29 12:55:37 +0200

    zmiana1

---
+++ lambdaa/lambdaa/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace lambdaa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tB1 = new System.Windows.Forms.TextBox();
+            this.tB2 = new System.Windows.Forms.TextBox();
+            this.rtB2 = new System.Windows.Forms.RichTextBox();
+            this.R1 = new System.Windows.Forms.RadioButton();
+            this.R2 = new System.Windows.Forms.RadioButton();
+            this.R3 = new System.Windows.Forms.RadioButton();
+            this.R4 = new System.Windows.Forms.RadioButton();
+            this.R5 = new System.Windows.Forms.RadioButton();
+            this.R6 = new System.Windows.Forms.RadioButton();
+            this.R7 = new System.Windows.Forms.RadioButton();
+            this.R8 = new System.Windows.Forms.RadioButton();
+            this.R8A = new System.Windows.Forms.RadioButton();
+            this.R9 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // tB1
+            // 
+            this.tB1.Location = new System.Drawing.Point(71, 122);
+            this.tB1.Name = "tB1";
+            this.tB1.Size = new System.Drawing.Size(100, 20);
+            this.tB1.TabIndex = 0;
+            // 
+            // tB2
+            // 
+            this.tB2.Location = new System.Drawing.Point(71, 164);
+            this.tB2.Name = "tB2";
+            this.tB2.Size = new System.Drawing.Size(100, 20);
+            this.tB2.TabIndex = 2;
+            // 
+            // rtB2
+            // 
+            this.rtB2.Location = new System.Drawing.Point(239, 67);
+            this.rtB2.Name = "rtB2";
+            this.rtB2.Size = new System.Drawing.Size(224, 224);
+            this.rtB2.TabIndex = 3;
+            this.rtB2.Text = "";
+            // 
+            // R1
+            // 
+            this.R1.AutoSize = true;
+            this.R1.Location = new System.Drawing.Point(535, 67);
+            this.R1.Name = "R1";
+            this.R1.Size = new System.Drawing.Size(73, 17);
+            this.R1.TabIndex = 4;
+            this.R1.TabStop = true;
+            this.R1.Text = "Zadanie 1";
+            this.R1.UseVisualStyleBackColor = true;
+            // 
+            // R2
+            // 
+            this.R2.AutoSize = true;
+            this.R2.Location = new System.Drawing.Point(535, 90);
+            this.R2.Name = "R2";
+            this.R2.Size = new System.Drawing.Size(73, 17);
+            this.R2.TabIndex = 6;
+            this.R2.TabStop = true;
+            this.R2.Text = "Zadanie 2";
+            this.R2.UseVisualStyleBackColor = true;
+            // 
+            // R3
+            // 
+            this.R3.AutoSize = true;
+            this.R3.Location = new System.Drawing.Point(535, 113);
+            this.R3.Name = "R3";
+            this.R3.Size = new System.Drawing.Size(73, 17);
+            this.R3.TabIndex = 8;
+            this.R3.TabStop = true;
+            this.R3.Text = "Zadanie 3";
+            this.R3.UseVisualStyleBackColor = true;
+            // 
+            // R4
+            // 
+            this.R4.AutoSize = true;
+            this.R4.Location = new System.Drawing.Point(535, 136);
+            this.R4.Name = "R4";
+            this.R4.Size = new System.Drawing.Size(73, 17);
+            this.R4.TabIndex = 10;
+            this.R4.TabStop = true;
+            this.R4.Text = "Zadanie 4";
+            this.R4.UseVisualStyleBackColor = true;
+            // 
+            // R5
+            // 
+            this.R5.AutoSize = true;
+            this.R5.Location = new System.Drawing.Point(535, 159);
+            this.R5.Name = "R5";
+            this.R5.Size = new System.Drawing.Size(73, 17);
+            this.R5.TabIndex = 12;
+            this.R5.TabStop = true;
+            this.R5.Text = "Zadanie 5";
+            this.R5.UseVisualStyleBackColor = true;
+            // 
+            // R6
+            // 
+            this.R6.AutoSize = true;
+            this.R6.Location = new System.Drawing.Point(535, 182);
+            this.R6.Name = "R6";
+            this.R6.Size = new System.Drawing.Size(73, 17);
+            this.R6.TabIndex = 14;
+            this.R6.TabStop = true;
+            this.R6.Text = "Zadanie 6";
+            this.R6.UseVisualStyleBackColor = true;
+            // 
+            // R7
+            // 
+            this.R7.AutoSize = true;
+            this.R7.Location = new System.Drawing.Point(535, 205);
+            this.R7.Name = "R7";
+            this.R7.Size = new System.Drawing.Size(73, 17);
+            this.R7.TabIndex = 16;
+            this.R7.TabStop = true;
+            this.R7.Text = "Zadanie 7";
+            this.R7.UseVisualStyleBackColor = true;
+            // 
+            // R8
+            // 
+            this.R8.AutoSize = true;
+            this.R8.Location = new System.Drawing.Point(535, 228);
+            this.R8.Name = "R8";
+            this.R8.Size = new System.Drawing.Size(79, 17);
+            this.R8.TabIndex = 18;
+            this.R8.TabStop = true;
+            this.R8.Text = "Zadanie 8a";
+            this.R8.UseVisualStyleBackColor = true;
+            // 
+            // R8A
+            // 
+            this.R8A.AutoSize = true;
+            this.R8A.Location = new System.Drawing.Point(535, 251);
+            this.R8A.Name = "R8A";
+            this.R8A.Size = new System.Drawing.Size(79, 17);
+            this.R8A.TabIndex = 20;
+            this.R8A.TabStop = true;
+            this.R8A.Text = "Zadanie 8b";
+            this.R8A.UseVisualStyleBackColor = true;
+            // 
+            // R9
+            // 
+            this.R9.AutoSize = true;
+            this.R9.Location = new System.Drawing.Point(535, 274);
+            this.R9.Name = "R9";
+            this.R9.Size = new System.Drawing.Size(73, 17);
+            this.R9.TabIndex = 22;
+            this.R9.TabStop = true;
+            this.R9.Text = "Zadanie 9";
+            this.R9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.R9);
+            this.Controls.Add(this.R8A);
+            this.Controls.Add(this.R8);
+            this.Controls.Add(this.R7);
+            this.Controls.Add(this.R6);
+            this.Controls.Add(this.R5);
+            this.Controls.Add(this.R4);
+            this.Controls.Add(this.R3);
+            this.Controls.Add(this.R2);
+            this.Controls.Add(this.R1);
+            this.Controls.Add(this.rtB2);
+            this.Controls.Add(this.tB2);
+            this.Controls.Add(this.tB1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tB1;
+        private System.Windows.Forms.TextBox tB2;
+        private System.Windows.Forms.RichTextBox rtB2;
+        private System.Windows.Forms.RadioButton R1;
+        private System.Windows.Forms.RadioButton R2;
+        private System.Windows.Forms.RadioButton R3;
+        private System.Windows.Forms.RadioButton R4;
+        private System.Windows.Forms.RadioButton R5;
+        private System.Windows.Forms.RadioButton R6;
+        private System.Windows.Forms.RadioButton R7;
+        private System.Windows.Forms.RadioButton R8;
+        private System.Windows.Forms.RadioButton R8A;
+        private System.Windows.Forms.RadioButton R9;
+    }
+}
+
